9f07c1db5e7469c70eb042110111331509aa1732
Adam French <adamfrench@Adams-Mac.local>
2013-02-27 18:22:38 +0000

    Initial Commit

---
+++ SkyAtlas/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SkyAtlas
+//
+//  Created by Adam French on 27/02/2013.
+//  Copyright (c) 2013 Adam French. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
